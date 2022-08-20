ff1097343eb652a90e459d8a484707dc620b9557
Jack Miller <jm0124@gadi-login-07.gadi.nci.org.au>
2022-08-20 11:10:02 +1000

    init library

--- src/initLibrary.py
+++ src/initLibrary.py
@@ -1,2 +1,29 @@
+import torch
+
 def gaussianElement(std, matrixSize):
-    return 1
+    sampler = torch.distributions.Normal(torch.Tensor([0]), torch.Tensor([std/matrixSize]))
+    Omega = sampler.sample((matrixSize, matrixSize))[..., 0]
+    
+    return Omega
+
+def gaussianElement(std, matrixSize):
+    gaussianMatrix = gaussianElement(std, matrixSize)
+    w, v = np.linalg.eig(Omega.cpu().detach().numpy())
+
+    w.real = np.random.uniform(-maxmin,maxmin, w.shape[0])
+    imag_dist = np.random.uniform(-maxmin,maxmin, w.shape[0])
+    w = w + np.zeros(w.shape[0], dtype=complex)
+    w.imag = imag_dist
+    
+    return Omega
+
+def gaussianElement(std, matrixSize):
+    gaussianMatrix = gaussianElement(std, matrixSize)
+    w, v = np.linalg.eig(Omega.cpu().detach().numpy())
+
+    w.real = np.random.uniform(-maxmin,maxmin, w.shape[0])
+    imag_dist = np.random.uniform(-maxmin,maxmin, w.shape[0])
+    w = w + np.zeros(w.shape[0], dtype=complex)
+    w.imag = imag_dist
+    
+    return Omega
