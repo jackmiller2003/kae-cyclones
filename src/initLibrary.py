87679655f09e7c0fcbbb56a599dc02ed328ff2e8
charlieoneill11 <cponeill00@gmail.com>
2022-08-20 15:07:11 +1000

    update

--- src/initLibrary.py
+++ src/initLibrary.py
@@ -15,18 +15,15 @@ def reconstruct_operator(w, v):
 def gaussianElement(std, matrixSize):
     sampler = torch.distributions.Normal(torch.Tensor([0]), torch.Tensor([std/matrixSize]))
     Omega = sampler.sample((matrixSize, matrixSize))[..., 0]
-    
     return Omega
 
 def gaussianEigen(std, matrixSize):
     Omega = gaussianElement(std, matrixSize)
     w, v = np.linalg.eig(Omega.cpu().detach().numpy())
-
     w.real = np.random.uniform(-std,std, w.shape[0])
     imag_dist = np.random.uniform(-std,std, w.shape[0])
     w = w + np.zeros(w.shape[0], dtype=complex)
     w.imag = imag_dist
-    
     return torch.from_numpy(reconstruct_operator(w,v).real).float()
 
 # def gaussianElement(std, matrixSize):
